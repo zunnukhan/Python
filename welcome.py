6daa4582009706a58ea5744d3d8a2f4f4d55a768
zunnukhan <hajira_khan_786@yahoo.com>
2025-02-16 20:10:47 +0530

    2

---
+++ welcome.py
@@ -0,0 +1,8 @@
+print("Hello welcome in the world of coding \n hi")
+print("hello hi ",1000)
+print("welcome to my",end="house")
+name=input("Enter your name")
+print("\n,helloh",name,"\n welcome to codingal")
+import keyword
+print("python keywords are.....")
+print(keyword.kwlist)
