41c121faba8b78cb642d1ae37db9d195dadb46cc
zunnukhan <hajira_khan_786@yahoo.com>
2025-02-23 20:25:28 +0530

    1

---
+++ data type .py
@@ -0,0 +1,15 @@
+a = 45
+print ("type of a:" ,type (a))
+b=22.46
+print("type of b:",type(b))
+c="zunnu"
+print("type of c :",type(c))
+d = True
+print("type of d :",type(d))
+print("\n After type casting....")
+a=str(a)
+print(a)
+print("Data type of a is ",type(a))
+b=int(b)
+print(b)
+print("Data type of b is",type(b))
---
+++ slice.py
@@ -0,0 +1,7 @@
+name="hello every one"
+slice_name =name[0:15]
+slice_name1=name[6:15:2]
+slice_name2=name[-1:-16:-1]
+print(slice_name)
+print(slice_name1)
+print(slice_name2)
