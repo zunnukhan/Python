2a43c37d210fd80f7679e9b36c0fe560f1679ac6
zunnukhan <hajira_khan_786@yahoo.com>
2025-02-26 20:32:05 +0530

    1

---
+++ persentage.py
@@ -0,0 +1,12 @@
+print("Enter marks obtained in 6 subjects :")
+maths=int(input("maths:"))
+english=int(input("english:"))
+hindi=int(input("hindi:"))
+sanskrit=int(input("sanskrit:"))
+social=int(input("social:"))
+science=int(input("science:"))
+sum=maths+english+hindi+sanskrit+social+science
+print ("sum of math,english,hindi,sanskrit,social and science")
+perc=(sum/600)*100
+print(end="Persentage marks=")
+print(perc)
