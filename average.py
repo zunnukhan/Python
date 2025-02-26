462e44b8a0adbc2b50030cb196c11b02e2d5595f
zunnukhan <hajira_khan_786@yahoo.com>
2025-02-26 20:31:16 +0530

    1

---
+++ average.py
@@ -0,0 +1,9 @@
+tree1=98
+tree2=94
+tree3=41
+tree4=95
+tree5=11
+sum=tree1+tree2+tree3+tree4+tree5
+print("The sum of all the 5 trees is",sum)
+average=sum/5
+print("The average of all the 5 treees is",average)
