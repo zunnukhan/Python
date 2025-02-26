c707c56de990ce5186ed8621c55046ad6a7ad106
zunnukhan <hajira_khan_786@yahoo.com>
2025-02-26 20:03:45 +0530

    1

--- acpdata types.py
+++ acpdata types.py
@@ -1,5 +1,7 @@
 dora="Doraemon is a fictional title character in the Japanese manga and anime eponymous series of the same name created by Fujiko ."
 dora1="Doraemon"
 slice_dora =dora[0:9]
-slice_dora=dora[47:55]
-print("congratulation",type (dora1))
+slice_dora1=dora[47:55]
+print(slice_dora1)
+print(slice_dora)
+print("congratulation",slice_dora)
