b2e63b6f726079a6c3f97c20f4e435b3ad992c40
zunnukhan <hajira_khan_786@yahoo.com>
2025-04-20 18:50:19 +0530

    1

---
+++ math oprator.py
@@ -0,0 +1,8 @@
+import math
+print('The floor and ceiling value of 23.56 are :'+ str(math.ceil(23.56))+',' + str (math.floor(23.56)))
+ 
+x=10
+y=-15
+print('The value of x after copying the sing from y is :'+ str(math.copysign(x,y)))
+print('Absolute value of  -96and 56 are '+ str(math.fabs(-96))+','+str(math.fabs(56)))
+print('The GCD of 24 and 56 : ' + str(math.gcd(24,56)))
