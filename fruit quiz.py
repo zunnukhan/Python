816db8b2d67d86b2ea017f12f6889e48474f7f7f
zunnukhan <hajira_khan_786@yahoo.com>
2025-06-04 20:58:34 +0530

    3

--- fruit quiz.py
+++ fruit quiz.py
@@ -10,19 +10,18 @@ class fruitquiz:
         
     def quiz(self):
         while(True):
-            fruit,color=
-random.choice(list(self.fruit.items()))
+            fruit,color=random.choice(list(self.fruit.items()))
             
-print("What is the color of{}".format(fruit))
-user_answer=input()
-if(user_answer.lower()==color):
-    print("Correct answer")
-else:
-    print("wrong answer")
+            print("What is the color of{}".format(fruit))
+            user_answer=input()
+            if(user_answer.lower()==color):
+                print("Correct answer")
+            else:
+              print("wrong answer")
 
-    option=int(input("Enter 0 if you wnt to play again otherwise enter 1: "))
-    if (option):
-        break
+            option=int(input("Enter 0 if you wnt to play again otherwise enter 1: "))
+            if (option):
+                break
 print("welcome to fruite quiz")
 fq=fruitquiz()
-fq.quiz
+fq.quiz()
